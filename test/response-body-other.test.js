71ed03ec5d27814d07c8656384418c96bc2ecca3
Hanif <hanif.hakim@spesolution.com>
2022-10-24 10:57:39 +0700

    response body sendFile

---
+++ test/response-body-other.test.js
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/contoh.txt');
+});
+
+test('Test Response', async () => {
+  const response = await request(app).get('/');
+  expect(response.text).toContain('This is sample text');
+});
